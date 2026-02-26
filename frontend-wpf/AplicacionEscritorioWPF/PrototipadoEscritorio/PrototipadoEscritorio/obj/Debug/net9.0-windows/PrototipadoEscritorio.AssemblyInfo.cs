6a3290ecfa68f0e171bc8e98076fcba300c77b3b
IvanAriass <ivan.arias@iesdoctorbalmis.com>
2026-02-26 12:56:48 +0100

    un poco de maquetación de accesorios

--- frontend-wpf/AplicacionEscritorioWPF/PrototipadoEscritorio/PrototipadoEscritorio/obj/Debug/net9.0-windows/PrototipadoEscritorio.AssemblyInfo.cs
+++ frontend-wpf/AplicacionEscritorioWPF/PrototipadoEscritorio/PrototipadoEscritorio/obj/Debug/net9.0-windows/PrototipadoEscritorio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrototipadoEscritorio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87808e10b23cb04624f6d15597c8acd938556b34")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68123b225a342d34793d6307f4062121ff8a7840")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrototipadoEscritorio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrototipadoEscritorio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
